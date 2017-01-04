e6e3faac0e5ba3606508971e6324c025c3304b8b
g.rengaraj <g.rengaraj@accenture.com>
2017-01-04 17:25:08 +0530

    Changes

--- WebContent/scripts/main.js
+++ WebContent/scripts/main.js
@@ -1,3 +1,15 @@
 $(document).load(function () {
-	alert($('.priority').text());
+	
+	alert('');
+	$('.priority').each(function () {
+		if($(this).text()=='Medium'){
+		  $(this).addClass('med');
+		 } else if($(this).text()=='Low') {
+		$(this).addClass('low');
+		}else if($(this).text()=='High') {
+		$(this).addClass('high');
+		} else{
+		$(this).addClass('normal');
+		};
+	});
 });
